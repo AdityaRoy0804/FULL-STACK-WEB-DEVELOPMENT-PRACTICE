08d5a7837c3b20bacbb5b2807f2c2f5f61b46944
Aditya Kumar Roy <roy97278@gmail.com>
2025-08-07 18:13:03 +0000

    Datatypes and Variables in Js

--- JavaScript-Practice/01_test.js
+++ JavaScript-Practice/01_test.js
@@ -4,4 +4,14 @@
  const var1 = "Aditya";
  let var2 = "Roy";
  console.table([var1,var2]);
- 
+
+ // Datatypes in JS
+let name = "Aditya";
+let age = 18;
+let isLog = false;
+let state = null; // no value assigned
+//object model DOM
+console.table([name,age,isLog,state]);
+console.log(typeof age);
+console.log(typeof state); // not null but object
+console.log(typeof undefined);
