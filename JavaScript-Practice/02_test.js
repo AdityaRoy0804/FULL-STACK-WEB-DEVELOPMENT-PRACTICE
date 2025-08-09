1df275499d81a17e2c11fb634d56583633aacc0f
Aditya Kumar Roy <roy97278@gmail.com>
2025-08-09 04:28:37 +0000

    Operators Paradox and Strict Check

--- JavaScript-Practice/02_test.js
+++ JavaScript-Practice/02_test.js
@@ -1,12 +1,20 @@
 // Operations in JS
 
-let value = 3;
-let neg = -value;
-console.log(neg);
+//let value = 3;
+//let neg = -value;
+//console.log(neg);
 
 // precedence of datatypes and some paradox of JS
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
 
+// operators paradox with null as well as undefined
+console.log(null>0)
+console.log(null == 0)  // has different working method
+console.log(null >= 0)  // logical operators convert the null to 0 hence such results.
+
+// Strict check
+console.log("2" == 2)   //checks only value 
+console.log("2" === 2)  // checks value as well as data type
